d4d0d30590f600eb0ac027578fc6be7868b380bb
shekhtusdiq <shekhtusdiq@gmail.com>
2014-07-23 09:47:14 -0400

    Final For Ubuntu

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,28 @@ require spark::params
 	# Add path in global variable.
 	Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 	
+	# Add Group Spark
+	group { "spark":
+    		ensure => "present",
+    		gid => '1001',
+		#before => 'Download_tar',
+	}
+	
+	# Add User Spark
+	user { 'spark':
+  		ensure           => 'present',
+  		gid              => '1001',
+  		groups           => 'spark',
+  		home             => '/opt/spark',
+  		password         => '$6$5qu6iW.hdIp$N4TF2DYDkVd0S72OBNeUbEMuv8OOxBuUvsOtfDpc3Pe2/0fqdv.7R5lss7anNoNHYXwd49lK.Y3X5iUUEIN57/',
+  		password_max_age => '99999',
+  		password_min_age => '0',
+  		shell            => '/bin/bash',
+  		uid              => '1001',
+  		managehome       => true,
+  		require          => Group["spark"],
+	}
+
 	# To test param variables in /tmp/test.txt file.
 	file { "/tmp/test.txt":
 		ensure => 'file',
@@ -20,38 +42,45 @@ require spark::params
 	}
 
 	# Create Installation directory
-	file { "${spark::params::install_dir}":
-    		ensure => "directory",
-        	alias => "Base_dir",
-		before => Exec["Download_tar"],
-	}
+	#file { "${spark::params::install_dir}":
+    	#	ensure => "directory",
+        #	alias => "Base_dir",
+	#	before => Exec["Download_tar"],
+	#}
 
 	# Donwload tar file
 	exec { "wget $get_url}":
+		user => 'spark',
 		cwd => "/opt/spark/",
 		command => "wget ${spark::params::get_url}",
+		timeout => 1800,
+		tries   => 2,
 		creates => "/opt/spark/spark-1.0.1-bin-hadoop1.tgz",
 		alias => "Download_tar",
+		require => User["spark"],
 	}
 	
 	# Extract spark
 	exec { "tar zxvf spark-1.0.1-bin-hadoop1.tgz -C /opt/spark":
-    		cwd => "/opt/spark/",
+    		user => 'spark',
+		cwd => "/opt/spark/",
         	creates => "${spark::params::spark_home}",
 		alias => "extract",
 		require => Exec['Download_tar']
 	}
 	# Copy JRE
         file { "${spark::params::spark_home}/jre-7u55-linux-x64.gz":
-                ensure => 'file',
+                owner => 'spark',
+		group => 'spark',
+		ensure => 'file',
                 source => 'puppet:///modules/spark/jre-7u55-linux-x64.gz',
-        	#creates => "${spark::params::spark_home}/jre-7u55-linux-x64.gz",
                 alias => 'Copy_JRE',
                 require => Exec["extract"],
         }
 	# Extract JRE
 	exec { "tar zxvf jre-7u55-linux-x64.gz":
-                cwd => "${spark::params::spark_home}",
+		user => 'spark',
+		cwd => "${spark::params::spark_home}",
                 creates => "${spark::params::spark_home}/jre1.7.0_55",
                 alias => "Extract_JRE",
                 require => File['Copy_JRE']
@@ -86,7 +115,7 @@ require spark::params
                 require => Exec['extract']
         }
  	
-	# Copy spark-shell
+	# Copy spark env
         file { "${spark::params::spark_home}/conf/spark-env.sh":
                 mode => 755,
                 ensure => 'file',
