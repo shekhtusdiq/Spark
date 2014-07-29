67e5b5b35fffe39865ddae944453b897f5caf3b2
shekhtusdiq <shekhtusdiq@gmail.com>
2014-07-29 18:41:20 +0530

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -31,6 +31,9 @@ class spark::params {
 
 #Spark Slaves        
   $slaves = $::hostname ? {
-    	default => "sparkA, sparkB",
+    	default => [ "0.centos.pool.ntp.org",
+                     "1.centos.pool.ntp.org",
+                     "2.centos.pool.ntp.org", ]
+    	
   }
 }
